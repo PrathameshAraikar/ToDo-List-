68a9f216cd5e03c0e13c48ddb183a605a9e0d475
Prathamesh Araikar <prathamesharaikar11@gmail.com>
2022-01-14 13:56:05 +0530

    Initial Commit

---
+++ ToDo-SwiftUI/ToDo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_SwiftUIApp.swift
+//  ToDo-SwiftUI
+//
+//  Created by Prathamesh Araikar on 14/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
